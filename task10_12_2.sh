ecf002a2942e0be60b2d1205a6a13ddc2a24bab7
SofyaTavrovskaya <tavrovskayasofya87@gmail.com>
2018-05-12 19:37:42 +0300

    some changes

--- task10_12_2.sh
+++ task10_12_2.sh
@@ -18,7 +18,7 @@ echo "Installing Docker-CE and Compose"
 apt-get install docker-ce docker-compose -y
 
 envsubst < subjalt_template.cnf > subjalt.cnf
-envsubst '${NGINX_PORT}' < nginx_template.conf > etc/nginx.conf
+
 echo "Making dir for certs"
 mkdir -p certs/
 
@@ -33,16 +33,18 @@ fi
 echo "Generating root certificate"
 openssl req -x509 -new -nodes -key certs/root.key -sha256 -days 365\
        -out certs/root.crt\
-       -subj "/C=UA/ST=Kharkov/L=Kharkov/O=Mirantis/OU=dev_ops/CN=docker-host/"\
+       -subj "/C=UA/ST=Kharkov/L=Kharkov/O=Mirantis/OU=dev_ops/CN=sofya-K53E/"\
        -extensions v3_req\
        -config <(cat /etc/ssl/openssl.cnf; cat subjalt.cnf)
 
 echo "Generating web.key"
 openssl genrsa -out certs/web.key 2048
 echo "Generating web.ctr"
-openssl req -new -out certs/web.crt -key certs/web.key -subj "/C=UA/ST=Kharkov/L=Kharkov/O=Mirantis/OU=dev_ops/CN=nginx/"
+openssl req -new -out certs/web.csr\
+       -key certs/web.key\
+       -subj "/C=UA/ST=Kharkov/L=Kharkov/O=Mirantis/OU=dev_ops/CN=sofya-K53E/"
 openssl x509 -req\
-       -in certs/web.crt\
+       -in certs/web.csr\
        -CA certs/root.crt\
        -CAkey certs/root.key\
        -CAcreateserial\
